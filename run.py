d106749ae2c394166eb0fe63acebb19694d7685f
haipb <phan.ba.hai@rewardstyle.com>
2020-11-24 14:20:29 +0700

    updated

--- run.py
+++ run.py
@@ -3,37 +3,25 @@ import requests
 import time
 
 # ------------ configs ------------ #
-craw_steps = 199
+craw_steps = 2
 status = 200
 request_delay = 0.25
 
 # ------------ suport functions ------------ #
 
-def findFullName(_nameId,_filesave):
-    fullName = ''
-    with open(_filesave) as json_file:
-        data = json.load(json_file)
-
-        for p in data:
-            if _nameId == p['nameId']:
-                fullName = p['fullName']
-                print(fullName)
-
-    return fullName
-# end findFullName
+# function to add to JSON 
+def write_json(data, filename='data.json'): 
+    with open(filename,'w') as f: 
+        json.dump(data, f, indent=4) 
 
 def processCrawling(appId, filesave):
-    print("Start Crawling!!!")
 
-    parms =''
-    # url = 'https://inventories.cs.money/4.0/load_bots_inventory/{}?hasTradeLock=false&isStore=true&limit=60&offset={}'
-    url ='https://inventories.cs.money/4.0/load_bots_inventory/{}?hasTradeLock=false&limit=60&offset={}'
-
-    x= 1
-    
+    print("Start Crawling!!!")    
+    x= 1    
     temp =[]
     while x < craw_steps or status !=200:
         try:
+            url ='https://inventories.cs.money/4.0/load_bots_inventory/{}?hasTradeLock=false&limit=60&offset={}'
             response = requests.get(url.format(appId,str(x*60)))
             data  = json.dumps(response.json())
             status = response.status_code
@@ -65,7 +53,6 @@ def processCrawling(appId, filesave):
                         i+=1
                         
                     temp.append(d)
-
                     
                 # end if d is not None
 
@@ -79,39 +66,58 @@ def processCrawling(appId, filesave):
     # update fullName 
     print("Start Updating {} items!!!".format(len(temp)))
    
-    k=0
-    while k < len(temp):
-        try:
-            print('updating nameId {}'.format( temp[k]['nameId']))
+    with open('fullName.json') as json_file:
+        data_fname = json.load(json_file)     
 
-            # get full name in filesave
-            fname = findFullName(temp[k]['nameId'],filesave)
+        k=0    
+        while k < len(temp):
+            try:
+                print('updating nameId {}'.format( temp[k]['nameId']))
 
-            # get full name by crawling
-            if fname =='':
-                url_update = 'https://cs.money/skin_info?appId={}&id={}&isBot=true&botInventory=true'
-                response = requests.get(url_update.format(appId,str(temp[k]['id'])))
-                data  = json.dumps(response.json())
-                status = response.status_code            
-                
-                details = json.loads(data)
-                if 'fullName' in details:
-                    temp[k]['fullName']=details['fullName']
-                elif 'name' in details:
-                    temp[k]['fullName']=details['name']
+                fname =''
+                # get full name in filesave
+                for p in data_fname:
+                    if temp[k]['nameId'] == p['nameId']:
+                        fname = p['fullName']
+
+                # get full name by crawling
+                if fname =='':
+                    url_update = 'https://cs.money/skin_info?appId={}&id={}&isBot=true&botInventory=true'
+                    response = requests.get(url_update.format(appId,str(temp[k]['id'])))
+                    data  = json.dumps(response.json())
+                    status = response.status_code            
+                                    
+                    details = json.loads(data)
+                    if 'fullName' in details:
+                        temp[k]['fullName']=details['fullName']
+                    elif 'name' in details:
+                        temp[k]['fullName']=details['name']
+                    else:
+                        temp[k]['fullName']=details['id']
+                    
+                    # add missing fullName
+                    n = {}
+                    n['nameId'] = temp[k]['nameId']
+                    n['fullName'] = temp[k]['fullName.json']
+
+                    data_fname.append(n)
+
                 else:
-                    temp[k]['fullName']=details['id']
-            else:
-                temp[k]['fullName']= fname
-        except:
-            pass
+                    temp[k]['fullName']= fname
+                    
+            except:      
+                break
 
-        k+=1
-    # end while k < len(temp)
+            k+=1
+            
+        # end while k < len(temp)
 
-    # write json file
-    with open(filesave,'w') as f: 
-        json.dump(temp, f, indent=4) 
+    # end with open('fullName.json')
+
+    # write json file cs, dota
+
+    write_json(data_fname,'fullName.json')
+    write_json(temp,filesave)
 
 # end processCrawling
 
